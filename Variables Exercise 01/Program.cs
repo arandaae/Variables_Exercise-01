f186041e8f234af4dc004610861f033aea10fb60
Armando Aranda <arandaae19@gmail.com>
2022-04-22 23:17:58 -0500

    Completed all TODO's

--- Variables Exercise 01/Program.cs
+++ Variables Exercise 01/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+
 
 namespace Variables_Exercise_01
 {
@@ -10,6 +12,10 @@ namespace Variables_Exercise_01
             // - Once finished save, commit, and then push to GitHub!
 
             // TODO: Declare 3 constansts
+            const string firstName = "Armando";
+            const int birthYear = 1992;
+            const char middleInitial = 'E';
+
 
             #region Types
             // string
@@ -22,10 +28,31 @@ namespace Variables_Exercise_01
 
             // TODO: Declare a variable for each of the types above
             // Make sure to use C# naming convention best practices 
+            string lastName;
+            char grade;
+            bool pass;
+            int age;
+            double inches;
+            decimal account;
+
 
             // TODO: Initialize each of the variables with a value
+            lastName = "Aranda";
+            grade = 'A';
+            pass = true;
+            age = 30;
+            inches = 1.1;
+            account = 1.123456789M;
 
-            // TODO: Write out each of the variables to the Console 
+
+            // TODO: Write out each of the variables to the Console
+            Console.WriteLine(lastName);
+            Console.WriteLine(grade);
+            Console.WriteLine(pass);
+            Console.WriteLine(age);
+            Console.WriteLine(inches);
+            Console.WriteLine(account);
+
         }
 
     }
